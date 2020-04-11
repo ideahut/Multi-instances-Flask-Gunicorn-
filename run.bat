43ef8cf363188a1de35f15d9e4dba697a06fa07c
ideahut <thomson.na7pulu@gmail.com>
2020-04-11 17:17:50 +0700

    Change .gitignore

--- run.bat
+++ run.bat
@@ -1,10 +1,11 @@
 @echo off
 
-set PYTHON=D:\04_PROJECT\FLASK\api\.venv\Scripts\python.exe
-set GUNICORN=D:\04_PROJECT\FLASK\api\.venv\Scripts\gunicorn.exe
+set PYTHON=.venv\Scripts\python.exe
+set GUNICORN=.venv\Scripts\gunicorn.exe
 set WORKER=10
 set THREAD=10
 set PORTS=(34001 34002 34003 34004)
+mkdir logs
 
 (for %%i in %PORTS% do ( 
 	rem start "Flask API (Port: %%i)" %GUNICORN% --workers=%WORKER% --threads=%THREAD% --worker-class=gthread -b 0.0.0.0:%%i sample:app
